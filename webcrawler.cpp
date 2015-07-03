59d3b47ab983a5b017a918d11c646fb983a1bb09
yshiroya <yshiroya@purdue.edu>
2015-07-03 15:15:33 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -2,3 +2,16 @@
 #include "webcrawler.h"
 
 // Add your implementation here
+
+WebCrawler::WebCrawler(int maxUrls, int nInitialURLs,  const char ** initialURLs)
+{
+  // Allocate space for _urlArray
+  // insert the initialURls
+  // Update _maxUrls, _headURL and _tailURL
+  
+  _urlArray = new URLRecord[maxUrls + nInitialURLs];
+}
+
+int main() {
+	return 0;
+}
