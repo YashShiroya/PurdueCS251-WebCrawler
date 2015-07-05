a44a3aa5e17657342d93111303174590ed71eb32
yshiroya <yshiroya@purdue.edu>
2015-07-05 18:12:02 -0400

    Commit web-crawler

--- SimpleHTMLParser.cpp
+++ SimpleHTMLParser.cpp
@@ -45,6 +45,9 @@ SimpleHTMLParser::parse(char * buffer, int n)
 			else if (match(&b,"<FRAME ")) {
 				state = FRAME;
 			}
+			else if (match(&b,"<TITLE>")) {
+				state = TITLE;
+			}
 			else if	(match(&b,"<")) {
 				state = TAG;
 			}
