7c05ccced896ea091d7ef4409ba7530eba4dc66d
yshiroya <yshiroya@purdue.edu>
2015-07-07 03:20:04 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -129,10 +129,8 @@ WebCrawler::onContentFound(char character) {
 	
 	if(character == '[') {
 	buffer_start[strlen(buffer_start) - 1] = '\0';
-		buffer_m = strdup(buffer_start);
-		_urlArray[_headURL]._description = strdup(buffer_m);
-		memset (buffer_start,'\0',strlen(buffer_start));
-		memset (_buffer,'\0',strlen(_buffer));
+		//buffer_m = strdup(buffer_start);
+		_urlArray[_headURL]._description = strdup(buffer_start);
 		_buffer = buffer_start;
 	}
 	
