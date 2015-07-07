d3206f811004a1fbe57e983638b581e9c556847b
yshiroya <yshiroya@purdue.edu>
2015-07-07 04:18:02 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -128,24 +128,28 @@ WebCrawler::onContentFound(char character) {
 	strcpy(buffer_k,"");
 	
 	if(character == '[') {
+
 	buffer_start[strlen(buffer_start) - 3] = '\0';
+
 		strcpy(buffer_m,"\n");
 		strcat(buffer_m,"Description:");
 		strcat(buffer_m,buffer_start);
 		strcat(buffer_m,"\n");
 		strcat(description, buffer_m);
-		buffer_m = buffer_m_p;
-		_buffer = buffer_start;
+
+		//_buffer = buffer_start;
 	}
 	if(character == ']') {
+
 	buffer_start[strlen(buffer_start) - 3] = '\0';
+		
 		strcpy(buffer_k,"\n");
 		strcat(buffer_k,"Keywords:");
 		strcat(buffer_k,buffer_start);
 		strcat(buffer_k,"\n");
 		strcat(description, buffer_k);
-		buffer_k = buffer_k_p;
-		_buffer = buffer_start;
+
+		//_buffer = buffer_start;
 	}
 	if(character == '_') {
 		memset (buffer_start,'\0',strlen(buffer_start));
@@ -160,8 +164,6 @@ WebCrawler::onContentFound(char character) {
 	if(buffer_k == NULL || strlen(buffer_k) == 0) {
 		strcpy(buffer_k,"");
 	}
-	
-	
 
 	_urlArray[_headURL]._description = strdup(description); 	
 	
