ae4361aa08b3d2f036724f10cd6fac08aa6c1503
yshiroya <yshiroya@purdue.edu>
2015-07-07 02:45:11 -0400

    Commit web-crawler

--- SimpleHTMLParser.cpp
+++ SimpleHTMLParser.cpp
@@ -97,7 +97,7 @@ SimpleHTMLParser::parse(char * buffer, int n)
 			break;
 		}
 		case CONTENT: {
-			if (match(&b,"name=\"keywords\"")) {
+			if (match(&b,"/>")) {
 				// End script
 				onContentFound('[');
 				state = START;
