cfff7ad4eb9b47ab4f21b0f4b09382d34489b80b
yshiroya <yshiroya@purdue.edu>
2015-07-11 17:04:53 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -174,7 +174,7 @@ WebCrawler::onContentFound(char character) {
 	if(character == '{') {
 	//buffer_start[strlen(buffer_start) - 4] = '\0';
 		//strcpy(buffer_k,"\n");
-		//strcat(buffer_k,"Keywords:");
+		strcat(buffer_k,"Keywords:");
 		//strcat(buffer_k,buffer_start);
 		//strcat(buffer_k,"\n");
 
