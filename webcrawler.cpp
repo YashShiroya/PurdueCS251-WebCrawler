375b80257c3b602eb7feeb912d86e4cba2d4eed8
yshiroya <yshiroya@purdue.edu>
2015-07-18 16:17:03 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -472,7 +472,7 @@ WebCrawler::onContentFound(char character) {
 		char * desc_cpy_start = desc_cpy;
 		//printf("desc_cpy_start %s\n", desc_cpy_start);
 		while(*d) {
-			if(*d != '\n' && *d != ',') {
+			if(*d != '\n') {
 				*desc_cpy = *d;
 				desc_cpy++;
 			}
@@ -495,7 +495,7 @@ WebCrawler::onContentFound(char character) {
 				else fprintf(f," %s\n", removeNextLn(_urlArray[i]._title));
 
 				if((_urlArray[i]._description) == NULL) fprintf(f,"");
-				else fprintf(f," %s\n\n", removeNextLn(_urlArray[i]._description));
+				else fprintf(f," %s\n", removeNextLn(_urlArray[i]._description));
 
 				if((_urlArray[i]._keywords) == NULL) fprintf(f,"");
 				else fprintf(f," %s\n", removeNextLn(_urlArray[i]._keywords));
@@ -512,6 +512,7 @@ WebCrawler::onContentFound(char character) {
 
 
 	int main(int argc, const char ** argv) { 
+
 		const char ** urlSet = argv;
 		urlSet += 1;
 		int maxURLs = 10;
@@ -519,7 +520,6 @@ WebCrawler::onContentFound(char character) {
 		printf("urlSet %s\n", *urlSet);
 
 		WebCrawler * wCrawler = new WebCrawler(maxURLs, argc - 1, urlSet);
-		
 		int i = 0;
 
 		//wCrawler->setTail(argc - 1);
