3b6c45b706df3255019e79a44c5926a1d77eb0a8
yshiroya <yshiroya@purdue.edu>
2015-07-18 15:56:20 -0400

    Commit web-crawler

--- webcrawler.cpp
+++ webcrawler.cpp
@@ -492,13 +492,13 @@ WebCrawler::onContentFound(char character) {
 				fprintf(f,"%d %s\n",i + 1 ,_urlArray[i]._url);
 
 				if((_urlArray[i]._title) == NULL) fprintf(f,"");
-				else fprintf(f," %s", removeNextLn(_urlArray[i]._title));
+				else fprintf(f," %s\n", removeNextLn(_urlArray[i]._title));
 
 				if((_urlArray[i]._description) == NULL) fprintf(f,"");
-				else fprintf(f," %s", removeNextLn(_urlArray[i]._description));
+				else fprintf(f," %s\n", removeNextLn(_urlArray[i]._description));
 
 				if((_urlArray[i]._keywords) == NULL) fprintf(f,"");
-				else fprintf(f," %s", removeNextLn(_urlArray[i]._keywords));
+				else fprintf(f," %s\n", removeNextLn(_urlArray[i]._keywords));
 
 				fprintf(f,"\n\n");			
 
